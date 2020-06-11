b829680dddfad49ad526b9513b8e1414d16823b8
Aditya Raina <adityaraina060199@gmail.com>
2020-06-11 23:02:22 +0530

    Files Added Successfully

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Navbar,NavbarBrand, Button } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component{
+render(){
+return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color = "primary">
+      <div className = "container">
+        <NavbarBrand href = "/"> Brand</NavbarBrand>
+       </div>
+      </Navbar>
+      <hr />
+      <div>
+         <Button color="primary" href = "https://www.google.com">Click!</Button>
+      </div>
     </div>
-  );
+);
 }
-
+}
 export default App;
